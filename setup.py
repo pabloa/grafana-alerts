a46fef17aa447065b2cb8202743a2f46aa728e0d
Pablo Alcaraz <pabloa98@gmail.com>
2015-06-16 19:10:59 -0700

    Changed version to 1.0.0.dev5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0.dev4',
+    version='1.0.0.dev5',
 
     description='Alerting Module to use with Grafana metrics.',
     long_description=long_description,
